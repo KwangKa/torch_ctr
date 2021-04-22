ffd507808055e0731616f5c09fcfe5fa73ed5733
kaka <eechengy@gmail.com>
2021-04-22 14:54:57 +0800

    add early stop

--- train.py
+++ train.py
@@ -3,23 +3,27 @@
 # @Author  : kaka
 
 import argparse
+import logging
 from tqdm import tqdm
 import torch
 from sklearn.metrics import roc_auc_score
+
 from dataset.util import get_data
 from model.lr import LR
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--model_name", type=str, default="lr", help="lr|fm|ffm|deepfm")
     parser.add_argument("--device", type=str, default="cpu", help="cpu|cuda:0")
     parser.add_argument("--datapath", type=str, default="./data/criteo_sampled_data_10k.csv", help="criteo data path")
     parser.add_argument("--num_workers", type=int, default=0, help="data loader worker num")
     parser.add_argument("--min_thres", type=int, default=10, help="feature count less than min_thres will be merged")
-    parser.add_argument("--batch_size", type=int, default=128, help="batch_size")
-    parser.add_argument("--epoch", type=int, default=20, help="epoch")
-    parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
+    parser.add_argument("--log_interval", type=int, default=10, help="display loss during training")
+    parser.add_argument("--early_stop_tolerance", type=int, default=3, help="early stop")
+    parser.add_argument("--batch_size", type=int, default=256, help="batch_size")
+    parser.add_argument("--epoch", type=int, default=100, help="epoch")
+    parser.add_argument("--lr", type=float, default=0.005, help="learning rate")
     parser.add_argument("--weight_decay", type=float, default=1e-6, help="weight decay")
     args = parser.parse_args()
     return args
@@ -37,10 +41,12 @@ def train_model(model, train_iter, val_iter, device, args):
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     criterion = torch.nn.BCELoss()
     model.to(device)
+    best_auc = 0.0
+    tolerance = 0
     for epoch_idx in range(1, args.epoch + 1):
         model.train()
         tk0 = tqdm(train_iter, smoothing=0, mininterval=1.0)
-        total_loss = 0
+        total_loss = 0.0
         for i, (fea, label) in enumerate(tk0):
             optimizer.zero_grad()
             fea.to(device)
@@ -50,10 +56,21 @@ def train_model(model, train_iter, val_iter, device, args):
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
-            if (i + 1) % 10 == 0:
-                tk0.set_postfix(loss=total_loss / 10)
+            if (i + 1) % args.log_interval == 0:
+                tk0.set_postfix(loss=total_loss / args.log_interval)
+                total_loss = 0.0
         auc = eval_model(model, val_iter, device)
-        print("epoch {0}, auc {1:.4f}".format(epoch_idx, auc))
+        logging.info("epoch {0}, auc {1:.4f}".format(epoch_idx, auc))
+        if args.early_stop_tolerance > 0:
+            # early stop check
+            if auc > best_auc:
+                best_auc = auc
+                tolerance = 0
+            else:
+                tolerance += 1
+                if tolerance > args.early_stop_tolerance:
+                    logging.info("Early stop")
+                    return
 
 
 def eval_model(model, data_iter, device):
@@ -72,14 +89,19 @@ def eval_model(model, data_iter, device):
 
 
 def main():
+    log_fmt = "%(asctime)s|%(name)s|%(levelname)s|%(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
     args = parse_args()
-    print(args)
+    logging.info(args)
+
+    device = torch.device(args.device)
     train_iter, val_iter, test_iter, field_dims = get_data(args)
     model = get_model(args, field_dims)
-    device = torch.device(args.device)
     train_model(model, train_iter, val_iter, device, args)
+
     test_auc = eval_model(model, test_iter, device)
-    print("\ntest auc :{0:.4f}".format(test_auc))
+    logging.info("test auc :{0:.4f}".format(test_auc))
 
 
 if __name__ == "__main__":
